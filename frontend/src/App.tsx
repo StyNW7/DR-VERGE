557fd72c58d9f196e07c7facfc06bc97dc325e06
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2026-08-11 15:01:51 +0700

    feat: setup dr-verge frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,55 @@
+// Default Import
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+
+import { Toaster } from "react-hot-toast";
+
+// Layout
+
+import Layout from "@/layouts/root-layout";
+
+// Utility Pages / Components
+
+import ScrollToTop from "./utility/ScrollToTop";
+import ScrollToTopFunction from "./utility/ScrollToTopFunction";
+import NotFoundPage from "./pages/Utility/NotFound404";
+
+// Pages
+
+import LandingPage from "@/pages/Landing/page";
+
+function App() {
+
+  return (
+
+    // Providers, Router, Scroll to Top Function and Button, and Custom Cursor
+
+    <BrowserRouter>
+      <ScrollToTopFunction />
+      <ScrollToTop />
+
+      <AnimatePresence mode="wait">
+
+          <Routes>
+
+            <Route path="/" element={<Layout />}>
+              
+                <Route index element={<LandingPage/>} />
+
+            </Route>
+
+            <Route path="*" element={<NotFoundPage />} />
+
+          </Routes>
+
+      </AnimatePresence>
+
+      <Toaster position="top-center" />
+
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
